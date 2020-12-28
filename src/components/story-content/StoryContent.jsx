a2a839ddf0fda46f4a1d22f179d7737262c5a984
Cristian <cristian.buciu89@gmail.com>
2020-12-28 06:29:40 +0100

    finished the story part and added gsap for scrolling

--- src/components/story-content/StoryContent.jsx
+++ src/components/story-content/StoryContent.jsx
@@ -1,12 +1,35 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import "./StoryContent.scss";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 //!===========================================================
 const StoryContent = () => {
+  const storyRef = useRef(null);
   gsap.registerPlugin(ScrollTrigger);
+
+  useEffect(() => {
+    gsap.to(
+      storyRef.current,
+      {
+        scrollTrigger: {
+          trigger: storyRef.current,
+          start: "200px 80%",
+          end: "bottom bottom",
+          toggleActions: "restart none reverse reverse",
+          // markers: true,
+          scrub: 1,
+        },
+        opacity: 1,
+
+        x: "0",
+        // duration: 1,
+      },
+      []
+    );
+  }, []);
+
   return (
-    <div className="story__content">
+    <div ref={storyRef} className="story__content">
       <h3 className="heading-3 mb-sm">Happy Customers</h3>
       <h2 className="heading-2 heading-2--dark mb-md">
         &ldquo;The best decision of our lives&rdquo;
--- src/components/story-pictures/StoryPictures.jsx
+++ src/components/story-pictures/StoryPictures.jsx
@@ -1,8 +1,42 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import "./StoryPictures.scss";
+import image1 from "../../assets/story-1.jpeg";
+import image2 from "../../assets/story-2.jpeg";
+
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 //!=========================================================
 const StoryPictures = () => {
-  return <div className="story__pictures">hello pictures</div>;
+  gsap.registerPlugin(ScrollTrigger);
+  const picturesRef = useRef(null);
+
+  useEffect(() => {
+    gsap.to(
+      picturesRef.current,
+      {
+        scrollTrigger: {
+          trigger: picturesRef.current,
+          start: "200px 80%",
+          end: "bottom bottom",
+          toggleActions: "restart none reverse reverse",
+          // markers: true,
+          scrub: 1,
+        },
+        opacity: 1,
+
+        x: "0",
+        // duration: 1,
+      },
+      []
+    );
+  }, []);
+
+  return (
+    <div ref={picturesRef} className="story__pictures">
+      <img src={image1} alt="a couple" className="story__img--1" />
+      <img src={image2} alt="a house" className="story__img--2" />
+    </div>
+  );
 };
 
 export default StoryPictures;

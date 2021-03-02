3bc2beba463b9331b0dba86579520ccc96e7f3ef
CristianBuciu <cristian.buciu89@gmail.com>
2021-03-02 08:45:47 +0100

    1. Added the header section

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,28 @@
 import React from "react";
 import "./Header.scss";
+import logo from "../../assets/logo.png";
+import logoBbc from "../../assets/logo-bbc.png";
+import logoBi from "../../assets/logo-bi.png";
+import logoForbes from "../../assets/logo-forbes.png";
+import logoTechCrunch from "../../assets/logo-techcrunch.png";
 //!=========================================================
 const Header = () => {
-  return <header className="header">hello from header</header>;
+  return (
+    <header className="header">
+      <img className="header__logo" src={logo} alt="logo" />
+      <h3 className="heading-3 header__h3">Your own home</h3>
+      <h1 className="heading-1">The ultimate personal freedom</h1>
+      <button className="button header__button">View our properties</button>
+      <div className="header__seenon-text">As seen on </div>
+      <div className="header__seenon-logos">
+        <img src={logoBbc} alt="logo BBC" />
+
+        <img src={logoForbes} alt="logo Forbes" />
+        <img src={logoTechCrunch} alt="logo TechChrunch" />
+        <img src={logoBi} alt="logo Bi" />
+      </div>
+    </header>
+  );
 };
 
 export default Header;
